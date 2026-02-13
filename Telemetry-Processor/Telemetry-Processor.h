49c29f56e82d1112a973bdd6f96a00e3e6c5e95e
Joshua Jones <143196112+joshjones49@users.noreply.github.com>
2026-02-13 16:20:19 -0500

    Created TelemetryProcessor.h

--- Telemetry-Processor/Telemetry-Processor.h
+++ Telemetry-Processor/Telemetry-Processor.h
@@ -1,8 +1,27 @@
-﻿// Telemetry-Processor.h : Include file for standard system include files,
-// or project specific include files.
+﻿#ifndef TP_H
+#define TP_H
 
-#pragma once
+#include "Sensor.h"
+#include "Logger.h"
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <thread>
 
-#include <iostream>
+class TelemetryProcessor {
+public:
+	TelemetryProcessor();
+	~TelemetryProcessor() = default;
 
-// TODO: Reference additional headers your program requires here.
+	void addSensor(std::unique_ptr<Sensor> sensor);
+	void runSim(int num_cycles = 20, int delay_ms = 300);
+
+private:
+	std::vector<std::unique_ptr<Sensor>> sensors_;
+	Logger logger_;
+
+	void processAllSensors();
+	void checkThresholds(double value, const std::string& name, const std::string& type);
+};
+
+#endif
