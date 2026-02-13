1de7052936d5c2bdc407ff583e82eb20141d7796
Joshua Jones <143196112+joshjones49@users.noreply.github.com>
2026-02-13 05:02:26 -0500

    Initial Commit

---
+++ Telemetry-Processor/Telemetry-Processor.cpp
@@ -0,0 +1,12 @@
+﻿// Telemetry-Processor.cpp : Defines the entry point for the application.
+//
+
+#include "Telemetry-Processor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
